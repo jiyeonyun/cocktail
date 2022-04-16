2cb82b40b64ea91bf617c4a92e9e1649d0254f37
jiyeonyun <jiyeonyun43@gmail.com>
2022-04-16 16:25:16 +0900

    random

--- src/app.jsx
+++ src/app.jsx
@@ -4,17 +4,34 @@ import Header from './components/header/header';
 
 function App({cocktail}) {
   const [rCocktail,setRcocktail] = useState();
+  const [randoms,setRandoms] = useState(false);
   const random = ()=>{
     cocktail
     .random()
-    .then(drink=>setRcocktail(drink));
+    .then(drink=>setRcocktail(drink[0]));
+    setRandoms(true);
   }
-  console.log(rCocktail);
-    
   return (
     <div className={styles.div}>
       <Header/>
       <img className={styles.img} src="./pngegg.png" alt="coktail image" />
+      {
+        randoms && 
+        <div className={styles.random}>
+          <img src={rCocktail.strDrinkThumb} alt="" />
+          <div className={styles.randomDes}>
+            <h2 className={styles.randomTitle}>{rCocktail.strDrink}</h2>
+            <ul className={styles.randomList}>
+              <li className={styles.randomLi}>Category: {rCocktail.strCategory}</li>
+              <li className={styles.randomLi}>Alcohol: {rCocktail.strAlcoholic}</li>
+              <li className={styles.randomLi}>Ingredient: {rCocktail.strIngredient1},
+              {rCocktail.strIngredient2},{rCocktail.strIngredient3},{rCocktail.strIngredient4}
+              {rCocktail.strIngredient5&&`,${rCocktail.strIngredient5}`} 
+              </li>
+            </ul>
+          </div>
+        </div>
+      }
       <div className={styles.buttons}>
       <button className={styles.button} onClick={random}>🐹Random Drinks🐹</button>
       <button className={styles.button}>🐰Go To Main🐰</button>
