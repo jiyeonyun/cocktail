42ea3ccbf18e3e4213f431d8625d520f752cae6c
jiyeonyun <jiyeonyun43@gmail.com>
2022-04-18 01:46:14 +0900

    build

--- src/app.jsx
+++ src/app.jsx
@@ -24,7 +24,7 @@ function App({cocktail}) {
 },[]);
   return (
     <div className={!night? styles.div : styles.Ndiv }>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Header/>
       <Routes>
           <Route exact path='/' element={<Main cocktail={cocktail}/>} />
